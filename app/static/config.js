5045d38dd46bb666dc55f221ad989a23943d132d
VahanG <vahangevorgyan93@gmail.com>
2017-11-02 10:47:13 +0400

    adding  avatar

--- app/static/config.js
+++ app/static/config.js
@@ -1,6 +1,6 @@
 const config = {
     sflAvatarUrl: "http://status.sflpro.com/assets/avatars/",
-    EMPTY_AVATAR: "assets/icon-192x192.png",
+    EMPTY_AVATAR: "assets/avatar.png",
 };
 
 export const { EMPTY_AVATAR, sflAvatarUrl, test } = config;
