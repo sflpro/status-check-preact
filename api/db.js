244549eb25c1a6b5f95b439016a82027baecbccd
Gagik Arustamyan <g.arustamyan@gmail.com>
2018-03-11 22:38:22 +0400

    Change the DB related configurations

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-    host: process.env.POSTGRES_HOST,
-    dialect: 'postgres',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
     operatorsAliases: false,
     pool: {
         max: 5,
--- postgres/init.sh
+++ postgres/init.sh
@@ -2,5 +2,5 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE rozaroute;
+    CREATE DATABASE $DB_NAME;
 EOSQL
