19464092f24c2150deb4cf15e548818124511b4f
Lilit Tadevosyan <lilit.tadevosyan@sflpro.com>
2018-03-14 15:29:16 +0400

    add logic of posting data in database

--- scheduler/index.js
+++ scheduler/index.js
@@ -1,98 +1,110 @@
+const logger = require('./logger');
 const xmlrpc = require('xmlrpc');
 const { promisify } = require('util');
-const logger = require('./logger');
+const requestPromise = require('request-promise-native');
+
+const env = process.env;
 
 const xmlRpcClient = xmlrpc.createClient({
-    host: process.env.TIMECARD_HOST,
+    host: env.TIMECARD_HOST,
     port: 3003,
-    path: process.env.TIMECARD_PATH,
+    path: env.TIMECARD_PATH,
     basic_auth: {
-        user: process.env.TIMECARD_USER,
-        pass: process.env.TIMECARD_PASS,
+        user: env.TIMECARD_USER,
+        pass: env.TIMECARD_PASS,
     },
 });
 
 const methodCall = promisify(xmlRpcClient.methodCall.bind(xmlRpcClient));
 
 function fetchEmployees() {
-    let employeesShort;
-    return methodCall('GetAllEmployeesShort', [])
-        .then(employees => {
-            let empls = employees.map(employee => ({
-                code: employee.Code,
-                badge: employee.Badge,
-                firstName: employee.Name,
-                middleName: employee.MiddleName,
-                lastName: employee.LastName,
-                fullName: employee.FullName,
-                active: employee.Active,
-            }));
-            return employees;
-        })
-        .then(employees => employees.filter(employee => employee.Active))
-        .then(employees => employees.map(employee => ({
-            id: employee.Id,
-            name: employee.FullName
-        })))
-        .then(employees => {
-            employeesShort = employees;
-            const startDate = new Date();
-            const endDate = new Date();
-            startDate.setDate(startDate.getDate() - 3);
-            endDate.setDate(endDate.getDate() + 1);
-            return methodCall('GetTransactions', [employees.map(employee => employee.id), startDate, endDate])
-        })
-        .then(transactions => {
-            let tactions = [];
+    return methodCall('GetAllEmployeesShort', []);
+}
 
-            transactions.forEach(transactionPerEmployee => {
-                transactionPerEmployee.Transactions.forEach(transaction => {
-                    let { Id, Action, DeviceId, Status, InsertDate, OriginalDate } = transaction;
-                    tactions.push({
-                        id: Id,
-                        employeeId: transactionPerEmployee.EmployeeId,
-                        action: Action,
-                        deviceId: DeviceId,
-                        status: Status,
-                        insertDate: InsertDate,
-                        originalDate: OriginalDate,
-                    });
-                });
-            });
+function fetchTransactions(employees) {
+    const startDate = new Date();
+    const endDate = new Date();
+    startDate.setDate(startDate.getDate() - env.REQUEST_FROM);
+    endDate.setDate(endDate.getDate() + env.REQUEST_TO);
+    return methodCall('GetTransactions', [employees.map(employee => employee.id), startDate, endDate]);
+}
 
-            return transactions;
-        })
-        .then(transactions => {
-            return transactions.reduce((accumulator, currentValue) => {;
-                const id = currentValue.EmployeeId;
-                const transactions = currentValue.Transactions;
-                const lastTransaction = transactions[transactions.length - 1];
+function sendEmployees(employees) {
+    const empls = employees.map(employee => ({
+        code: employee.Code,
+        badge: employee.Badge,
+        firstName: employee.Name,
+        middleName: employee.MiddleName,
+        lastName: employee.LastName,
+        fullName: employee.FullName,
+        active: employee.Active,
+    }));
+    const options = {
+        method: 'POST',
+        uri: `${env.API_ENDPOINT}/employees`,
+        body: empls,
+        json: true,
+    };
+    return requestPromise(options);
+}
 
-                accumulator[id] = {};
-                accumulator[id].status = lastTransaction.DeviceId === 3 ? 'in' : 'out';
-                accumulator[id].lastStatusChange = lastTransaction.InsertDate;
-                return accumulator;
-            }, {});
-        })
-        .then(transactions => {
-            return employeesShort.map(employee => {
-                employee.status = transactions[employee.id] ? transactions[employee.id].status : 'out';
-                employee.lastStatusChange = transactions[employee.id] ? transactions[employee.id].lastStatusChange : null;
-                return employee;
+function sendTransactions(transactions) {
+    let tactions = [];
+    transactions.forEach(transactionPerEmployee => {
+        transactionPerEmployee.Transactions.forEach(transaction => {
+            let { Id, Action, DeviceId, Status, InsertDate, OriginalDate } = transaction;
+            tactions.push({
+                id: Id,
+                employeeId: transactionPerEmployee.EmployeeId,
+                action: Action,
+                deviceId: DeviceId,
+                status: Status,
+                insertDate: InsertDate,
+                originalDate: OriginalDate,
             });
         });
+    });
+    const options = {
+        method: 'POST',
+        uri: `${env.API_ENDPOINT}/transactions`,
+        body: tactions,
+        json: true,
+    };
+    return requestPromise(options);
 }
 
 function start() {
+    logger.info('Fetching employees starts');
+    let employees;
     fetchEmployees()
-        .then(result => {
-            console.log('Employees are successfully fetched');
-            setTimeout(start, 10000);
+        .then(empls => {
+            logger.info('Fetching employees succeed');
+            employees = empls;
+            return empls.map(employee => ({
+                id: employee.Id,
+                name: employee.FullName
+            }))
+        })
+        .then(empls => {
+            logger.info('Fetching transaction starts');
+            return fetchTransactions(empls);
+        })
+        .then(transactions => {
+            logger.info('Fetching transaction succeed');
+            logger.info('Saving employees and transactions starts');
+            return Promise.all([sendEmployees(employees), sendTransactions(transactions)]);
+        })
+        .then(() => {
+            logger.info('Saving employees and transactions succeed');
+            setTimeout(start, env.REQUEST_INTERVAL);
         })
         .catch(err => {
-            console.error('Error fetching the list of employees', err)
-            setTimeout(start, 10000);
+            logger.error('fetching or saving failes', err);
+            setTimeout(start, env.REQUEST_INTERVAL);
         });
 }
 
+logger.info('Running jobs start.');
 start();
+
+
