c3fd1d09ca60db81484bec0ab874d68f80fa1fc1
Khachatur Tovmasyan <khachatur.tovmasyan@sflpro.com>
2018-06-15 17:11:11 +0400

    added tab navigations

--- native/App.js
+++ native/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator} from 'react-navigation'; // Version can be specified in package.json
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Status Check - SFL LLC 2018.</Text>
-      </View>
-    );
-  }
+class InOffice extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>In Office</Text>
+            </View>
+        );
+    }
 }
 
+class OutOfOffice extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Out of Office</Text>
+            </View>
+        );
+    }
+}
+
+export default createMaterialTopTabNavigator({
+    In: { screen: InOffice },
+    Out: { screen: OutOfOffice },
+});
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- native/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $NODE_ENV == 'development' ]
-then
-    nodemon index.js
-else
-    node index.js
-fi
