1e1d8d8265b66e1352c205165b2fd0a0fdfd9cad
Lilit Tadevosyan <lilit.tadevosyan@sflpro.com>
2018-06-20 18:35:40 +0400

    Fix logic of defining secrets and their usage

--- scheduler/start.sh
+++ scheduler/start.sh
@@ -6,4 +6,9 @@ then
     nodemon src
 else
     node src
+    export TIMECARD_HOST = $(cat $TIMECARD_HOST_FILE)
+    export TIMECARD_PATH = $(cat $TIMECARD_PATH_FILE)
+    export TIMECARD_USER = $(cat $TIMECARD_USER_FILE)
+    export TIMECARD_PASS = $(cat $TIMECARD_PASS_FILE)
+    node src
 fi
